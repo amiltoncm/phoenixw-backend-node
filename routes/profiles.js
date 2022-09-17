49218563caff577f798be37692afb1d13a8aaae3
Amilton Machado <amilton.machado@fw7.com.br>
2022-09-17 09:13:58 -0300

    feat(profiles) added getById

--- routes/profiles.js
+++ routes/profiles.js
@@ -2,12 +2,13 @@ module.exports = app => {
   
   // Profiles
   app.route('/profiles')
-    //.all(app.config.passport.authenticate())
+    .all(app.config.passport.authenticate())
     .post(app.api.profile.save)
     .get(app.api.profile.get);
   
   app.route('/profiles/:id')
-    //.all(app.config.passport.authenticate())
+    .all(app.config.passport.authenticate())
+    .get(app.api.profile.getById)
     .put(app.api.profile.save)
     .delete(app.api.profile.del);
   
