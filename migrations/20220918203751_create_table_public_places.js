d272045df6cf38058454cbf1f1469168c4ebb741
Amilton Machado <amilton.machado@fw7.com.br>
2022-09-18 18:06:57 -0300

    feat(domains) added new tables

---
+++ migrations/20220918203751_create_table_public_places.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('public_places', function (table) {
+    table.increments('id');
+    table.string('name', 20).notNullable().unique();
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return;
+};
---
+++ migrations/20220918204047_create_table_person_type.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('person_type', function (table) {
+    table.increments('id');
+    table.string('name', 15).notNullable().unique();
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return;
+};
